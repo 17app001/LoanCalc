78fade40dee7be5ac17df7f1a3abd3ca7b9eda80
jerry <jerry@gmail.com>
2023-06-10 14:13:07 +0800

    完成函式宣告跟顯示分離

--- index.js
+++ index.js
@@ -3,50 +3,78 @@ const InterestRateEl = document.querySelector("#interestRate");
 const LoanTermEl = document.querySelector("#loanTerm");
 const resultEl = document.querySelector("#result");
 const tableResultEl = document.querySelector("#tableResult");
+const yearLoanTermEl = document.querySelector("#yearLoanTerm");
 
-console.log(tableResultEl);
+console.log(tableResultEl, yearLoanTermEl);
+console.log(calcResult(200000, 4.5, 10));
 
+/**
+ * 本金攤還計算公式
+ * @param {*} loanAmount 
+ * @param {*} interestRate 
+ * @param {*} loanTerm 
+ * @returns 
+ */
+function calcResult(loanAmount, interestRate, loanTerm) {
+    let resultArray = [];
+    let proid = loanTerm * 12;
+    let monthReplacement = parseInt(loanAmount / proid);
+    // 取得期數還款資訊
+    for (let i = 0; i < proid; i++) {
+        let interest = Math.round(loanAmount * (interestRate / 100 / 12));
+        resultArray[i] = new Array();
+        if (i == proid - 1) {
+            resultArray[i].push(i + 1);
+            resultArray[i].push(monthReplacement + loanAmount - monthReplacement);
+            resultArray[i].push(interest);
+            resultArray[i].push(monthReplacement + loanAmount - monthReplacement + interest);
+            resultArray[i].push(0);
+        }
+        else {
+            resultArray[i].push(i + 1);
+            resultArray[i].push(monthReplacement);
+            resultArray[i].push(interest);
+            resultArray[i].push(monthReplacement + interest);
+            resultArray[i].push(loanAmount - monthReplacement);
+        }
 
-function calcB() {
+        loanAmount -= monthReplacement;
+    }
+
+    return resultArray;
+}
+
+function insertDataToTable() {
+    // 清除rows
     while (tableResultEl.rows.length > 1) {
         tableResultEl.deleteRow(1);
     }
     // 貸款金額
-    let loanAmount = LoanAmountEl.value;
+    let loanAmount = LoanAmountEl.value * 10000;
     // 年利率
     let interestRate = InterestRateEl.value;
-    // 年限
-    let loanTerm = LoanTermEl.value;
-    console.log(loanAmount, interestRate, loanTerm);
-    let proid = loanTerm * 12;
-    let monthReplacement = parseInt(loanAmount / proid);
-    // 計算總利息
-    let totalInterest = 0;
-    // 取得期數還款資訊
-    for (let i = 0; i < proid; i++) {
-        let interest = Math.round(loanAmount * (interestRate / 100 / 12));
-        totalInterest += interest;
-        //取得列物件
-        let row = tableResultEl.insertRow();
-        if (i == proid - 1) {
-            //插入行物件
-            row.insertCell().innerText = i + 1;
-            row.insertCell().innerText = monthReplacement + loanAmount - monthReplacement;
-            row.insertCell().innerText = interest;
-            row.insertCell().innerText = monthReplacement + loanAmount - monthReplacement + interest;
-            row.insertCell().innerText = 0;
-        }
-        else {
-            row.insertCell().innerText = i + 1;
-            row.insertCell().innerText = monthReplacement;
-            row.insertCell().innerText = interest;
-            row.insertCell().innerText = monthReplacement + interest;
-            row.insertCell().innerText = loanAmount - monthReplacement;
-        }
-        loanAmount -= monthReplacement;
+    // 年期(三元運算子)   
+    let loanTerm = yearLoanTermEl.value != "" ? yearLoanTermEl.value : LoanTermEl.value;
+
+    if (loanTerm <= 0) {
+        resultEl.innerHTML = "貸款年限輸入不能小於等於零";
+        return;
     }
 
-    resultEl.innerHTML = `總支出:${Number(LoanAmountEl.value) + totalInterest} 利息:${totalInterest}`;
+    let resultArray = calcResult(loanAmount, interestRate, loanTerm);
+    let totalInterest = 0;
+
+    for (let i = 0; i < resultArray.length; i++) {
+        // 取得列物件
+        let row = tableResultEl.insertRow();
+        for (let j = 0; j < resultArray[i].length; j++) {
+            //插入行物件
+            row.insertCell().innerText = resultArray[i][j];
+        }
+        totalInterest += resultArray[i][2];
+    }
+
+    resultEl.innerHTML = `總期數:${resultArray.length} 總支出:${Number(LoanAmountEl.value) + totalInterest} 利息:${totalInterest}`;
 }
 
 
